be4d3605158b3d352929cf4aa9bf968741ec4311
Omondi Ochieng <oa@omondi.co.za>
2023-02-04 13:50:56 +0200

    Add modal for game ending. Game done

---
+++ flordle/src/components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Modal({isCorrect, turn, solution}) {
+
+
+  return (
+    <div className='modal'>
+        {isCorrect && (
+            <div>
+                <h1>You win!</h1>
+                <p className='solution'>{solution}</p>
+                <p>You found the solution in {turn} guesses</p>
+            </div>
+        )}
+        {!isCorrect && (
+            <div>
+                <h1>Try again</h1>
+                <p className='solution'>The solution was {solution}</p>
+                <p>Better luck next time</p>
+            </div>
+        )}
+    </div>
+  )
+}
--- flordle/src/components/Wordle.js
+++ flordle/src/components/Wordle.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid.js'
 import Keypad from './Keypad.js'
+import Modal from './Modal'
 
 export default function Wordle({solution}) {
     const {currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys, NUMBER_OF_TURNS} = useWordle(solution)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
 
         // Stop processing key events if correct answer given or number of turns reached
         if (isCorrect || turn > NUMBER_OF_TURNS-1) {
+          setTimeout(()=> setShowModal(true), 2000)
           window.removeEventListener('keyup', handleKeyup)
         }
 
@@ -29,6 +32,7 @@ export default function Wordle({solution}) {
         <h3> Solution is = {solution}</h3>
         <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
         <Keypad usedKeys={usedKeys}/>
+        {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
   )
 }
